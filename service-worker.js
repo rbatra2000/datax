7c697566febd1024158760f079a0e06ac7a29b54
ritik.batra2000@gmail.com <ritik.batra2000@gmail.com>
2021-11-12 02:10:46 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/datax/precache-manifest.e4321e044de713b5902c975ccdfa23bf.js"
+  "/datax/precache-manifest.aac89ff719aa2ebaa02b36663e56f142.js"
 );
 
 self.addEventListener('message', (event) => {
